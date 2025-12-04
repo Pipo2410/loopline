00cb3aa9729f134b36d2938c9c167aff9199e601
Pipo2410 <Pipo2410@gmail.com>
2025-12-04 00:33:08 +0000

    chore(lint): Setup esLint

--- apps/web/app/(marketing)/page.tsx
+++ apps/web/app/(marketing)/page.tsx
@@ -1,6 +1,5 @@
 import { HeroHeader } from '@/app/(marketing)/_components/header'
 import { HeroSection } from '@/app/(marketing)/_components/hero-section'
-
 export default function Home() {
   return (
     <div>
