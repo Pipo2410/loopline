541a7cd0d494817b6205f517cbaf04992b1296e7
Pipo2410 <37326909+Pipo2410@users.noreply.github.com>
2025-12-06 19:04:03 +0000

    feat(auth): Setup auth with Kinde

--- apps/web/app/(marketing)/_components/header.tsx
+++ apps/web/app/(marketing)/_components/header.tsx
@@ -1,60 +1,72 @@
-'use client'
-import Link from 'next/link'
-import { Menu, X } from 'lucide-react'
-import { Button } from '@workspace/ui/components/button'
-import React from 'react'
-import { cn } from '@workspace/ui/lib/utils'
-import Logo from '@/public/logo.png'
-import Image from 'next/image'
+'use client';
+import { useKindeBrowserClient } from '@kinde-oss/kinde-auth-nextjs';
+import {
+  LoginLink,
+  LogoutLink,
+  RegisterLink,
+} from '@kinde-oss/kinde-auth-nextjs/components';
+import { buttonVariants } from '@workspace/ui/components/button';
+import { cn } from '@workspace/ui/lib/utils';
+import { Menu, X } from 'lucide-react';
+import Image from 'next/image';
+import Link from 'next/link';
+import React from 'react';
+
+import Logo from '@/public/logo.png';
 
 const menuItems = [
-  { name: 'Features', href: '#link' },
-  { name: 'Solution', href: '#link' },
-  { name: 'Pricing', href: '#link' },
-  { name: 'About', href: '#link' },
-]
+  { href: '#link', name: 'Features' },
+  { href: '#link', name: 'Solution' },
+  { href: '#link', name: 'Pricing' },
+  { href: '#link', name: 'About' },
+];
 
-export const HeroHeader = () => {
-  const [menuState, setMenuState] = React.useState(false)
-  const [isScrolled, setIsScrolled] = React.useState(false)
+const SCROLL_CONST = 50;
+
+export const HeroHeader: React.FC = () => {
+  const [menuState, setMenuState] = React.useState(false);
+  const [isScrolled, setIsScrolled] = React.useState(false);
+  const { isLoading, getUser } = useKindeBrowserClient();
+  const user = getUser();
 
   React.useEffect(() => {
-    const handleScroll = () => {
-      setIsScrolled(window.scrollY > 50)
-    }
-    window.addEventListener('scroll', handleScroll)
-    return () => window.removeEventListener('scroll', handleScroll)
-  }, [])
+    const handleScroll = (): void => {
+      setIsScrolled(window.scrollY > SCROLL_CONST);
+    };
+    window.addEventListener('scroll', handleScroll);
+    return (): void => window.removeEventListener('scroll', handleScroll);
+  }, []);
   return (
     <header>
       <nav
-        data-state={menuState && 'active'}
         className='fixed z-20 w-full px-2'
+        data-state={menuState && 'active'}
       >
         <div
           className={cn(
             'mx-auto mt-2 max-w-6xl px-6 transition-all duration-300 lg:px-12',
             isScrolled &&
-              'bg-background/50 max-w-4xl rounded-2xl border backdrop-blur-lg lg:px-5'
+              'bg-background/50 max-w-4xl rounded-2xl border backdrop-blur-lg lg:px-5',
           )}
         >
           <div className='relative flex flex-wrap items-center justify-between gap-6 py-3 lg:gap-0 lg:py-4'>
             <div className='flex w-full justify-between lg:w-auto'>
               <Link
-                href='/'
                 aria-label='home'
                 className='flex items-center space-x-2'
+                href='/'
               >
-                <Image src={Logo} alt='logo' width={32} height={32} />
+                <Image alt='logo' height={32} src={Logo} width={32} />
                 <h1 className='text-2xl font-bold'>
                   Loop<span className='text-primary'>Line</span>
                 </h1>
               </Link>
 
               <button
-                onClick={() => setMenuState(!menuState)}
+                // eslint-disable-next-line eqeqeq
                 aria-label={menuState == true ? 'Close Menu' : 'Open Menu'}
                 className='relative z-20 -m-2.5 -mr-4 block cursor-pointer p-2.5 lg:hidden'
+                onClick={() => setMenuState(!menuState)}
               >
                 <Menu className='in-data-[state=active]:rotate-180 in-data-[state=active]:scale-0 in-data-[state=active]:opacity-0 m-auto size-6 duration-200' />
                 <X className='in-data-[state=active]:rotate-0 in-data-[state=active]:scale-100 in-data-[state=active]:opacity-100 absolute inset-0 m-auto size-6 -rotate-180 scale-0 opacity-0 duration-200' />
@@ -66,8 +78,8 @@ export const HeroHeader = () => {
                 {menuItems.map((item, index) => (
                   <li key={index}>
                     <Link
-                      href={item.href}
                       className='text-muted-foreground hover:text-accent-foreground block duration-150'
+                      href={item.href}
                     >
                       <span>{item.name}</span>
                     </Link>
@@ -82,8 +94,8 @@ export const HeroHeader = () => {
                   {menuItems.map((item, index) => (
                     <li key={index}>
                       <Link
-                        href={item.href}
                         className='text-muted-foreground hover:text-accent-foreground block duration-150'
+                        href={item.href}
                       >
                         <span>{item.name}</span>
                       </Link>
@@ -91,40 +103,65 @@ export const HeroHeader = () => {
                   ))}
                 </ul>
               </div>
-              <div className='flex w-full flex-col space-y-3 sm:flex-row sm:gap-3 sm:space-y-0 md:w-fit'>
-                <Button
-                  asChild
-                  variant='outline'
-                  size='sm'
-                  className={cn(isScrolled && 'lg:hidden')}
-                >
-                  <Link href='#'>
-                    <span>Login</span>
-                  </Link>
-                </Button>
-                <Button
-                  asChild
-                  size='sm'
-                  className={cn(isScrolled && 'lg:hidden')}
-                >
-                  <Link href='#'>
-                    <span>Sign Up</span>
-                  </Link>
-                </Button>
-                <Button
-                  asChild
-                  size='sm'
-                  className={cn(isScrolled ? 'lg:inline-flex' : 'hidden')}
-                >
-                  <Link href='#'>
-                    <span>Get Started</span>
-                  </Link>
-                </Button>
-              </div>
+              {isLoading ? null : (
+                <div className='flex w-full flex-col space-y-3 sm:flex-row sm:gap-3 sm:space-y-0 md:w-fit'>
+                  {user ? (
+                    <>
+                      <Link
+                        className={buttonVariants({
+                          size: 'sm',
+                        })}
+                        href='/dashboard'
+                      >
+                        <span>Dashboard</span>
+                      </Link>
+                      <LogoutLink
+                        className={buttonVariants({
+                          size: 'sm',
+                          variant: 'outline',
+                        })}
+                      >
+                        <span>Logout</span>
+                      </LogoutLink>
+                    </>
+                  ) : (
+                    <>
+                      <LoginLink
+                        className={buttonVariants({
+                          className: cn(isScrolled && 'lg:hidden'),
+                          size: 'sm',
+                          variant: 'outline',
+                        })}
+                      >
+                        Login
+                      </LoginLink>
+                      <RegisterLink
+                        className={buttonVariants({
+                          className: cn(isScrolled && 'lg:hidden'),
+                          size: 'sm',
+                        })}
+                      >
+                        Sign up
+                      </RegisterLink>
+                      <div
+                        className={cn(isScrolled ? 'lg:inline-flex' : 'hidden')}
+                      >
+                        <RegisterLink
+                          className={buttonVariants({
+                            size: 'sm',
+                          })}
+                        >
+                          Get Started
+                        </RegisterLink>
+                      </div>
+                    </>
+                  )}
+                </div>
+              )}
             </div>
           </div>
         </div>
       </nav>
     </header>
-  )
-}
+  );
+};
